d4a21018395c9c13ddb2437ab357799233166b8f
Johnwickoo <bukolafaduagba@gmail.com>
2026-04-02 11:37:32 +0100

    Refactor:  Integrated Functions for both files

--- exercise1.py
+++ exercise1.py
@@ -2,36 +2,43 @@ import datetime
 import json
 
 study_log = {}
-
 current_date = datetime.datetime.now()
-todays_task = input("What did you do today?");
-todays_task = todays_task.title();
-duration = input("how many hours?");
-problems_solved = input("How many problems did you solve?");
 
-valid_ratings = (1,2,3,4,5)
-while True:
-	todays_rating = int(input("Rate your session 1-5"));
-	if todays_rating in valid_ratings:
-		 break
-	print("Please enter a number between 1 and 5")
+def get_user_input():
+	todays_task = input("What did you do todays?");
+	todays_task = todays_task.title();
+	duration = input("how many hours?");
+	problems_solved = input("How many problems did you solve?");
+
+	valid_ratings = (1,2,3,4,5)
+	while True:
+		todays_rating = int(input("Rate your session 1-5"));
+		if todays_rating in valid_ratings:
+			 break
+		print("Please enter a number between 1 and 5")
 	
-difficulties_encountered = input("Any blockers?")
-
-
-#study_log[current_date] the current date is the key, so basically its a nested dictionary, the value is another dictionary with the details of the session.
-study_log[current_date] = {
-	"task": todays_task,
-	"duration": duration,
-	"problems_solved": problems_solved,
-	"rating": todays_rating,
-	"difficulties": difficulties_encountered
-}
-
-print("Today's study log:", study_log[current_date])
-
-with open("study_log.txt", "a") as f:
-	f.write(json.dumps(study_log[current_date]) + "\n")
-	f.write("\n--- END OF ENTRY ---\n\n")
+	difficulties_encountered = input("Any blockers?")
 	
-print("Logged. Keep going.")
+	#study_log[current_date] the current date is the key, so basically its a nested dictionary, the value is another dictionary with the details of the session.
+	study_log[current_date] = {
+		"task": todays_task,
+		"duration": duration,
+		"problems_solved": problems_solved,
+		"rating": todays_rating,
+		"difficulties": difficulties_encountered
+	}
+	return study_log[current_date]
+
+
+
+# print("Today's study log:", study_log[current_date])
+
+def save_entry(entry):
+	with open("study_log.txt", "a") as f:
+		f.write(json.dumps(entry) + "\n")
+		f.write("\n--- END OF ENTRY ---\n\n")
+		
+	print("Logged. Keep going.")
+
+entry = get_user_input()
+save_entry(entry)
--- summary.py
+++ summary.py
@@ -1,38 +1,47 @@
 import json
 
-total_hours = 0
-total_problems = 0
-total_sessions = 0
-total_rating = 0
-total_blockers = []
+def load_entries():
+	entries = []
+	with open("study_log.txt","r",encoding = "utf-8") as f:
 
-with open("study_log.txt","r",encoding = "utf-8") as f:
-
-	for line in f:
-		line = line.strip()
-		if line.startswith("{") and line.endswith("}"):
-			entry = json.loads(line)
-			hours = int(entry.get("duration", 0))
-			total_hours += hours
-			problems = int(entry.get("problems_solved", 0))
-			total_problems += problems
-			rating = int(entry.get("rating", 0))
-			total_rating += rating
-			blocker = entry.get("difficulties", "")
-			if blocker:
-				total_blockers.append(blocker)
-			total_sessions += 1
-
-print("WEEKLY SUMMARY\n")
-print("--------\n")
-print(f"Total sessions logged:{total_sessions}\n")
-print(f"Total hours studied:{total_hours}\n")
-print(f"Total problems solved:{total_problems}\n")
-average_rating = total_rating / total_sessions if total_sessions > 0 else 0
-print(f"Average session rating:{average_rating}\n\n")
-print(f"BLOCKERS THIS WEEK:\n")
-for blockers in total_blockers:
-	print('. '+blockers)
+		for line in f:
+			line = line.strip()
+			if line.startswith("{") and line.endswith("}"):
+				entry = json.loads(line)
+				entries.append(entry)
+	return entries
 
 
+
+def print_summary(entries):
+	total_hours = 0
+	total_problems = 0
+	total_sessions = len(entries)
+	total_rating = 0
+	total_blockers = []
+
+	for entry in entries:
+		hours = int(entry.get("duration", 0))
+		total_hours += hours
+		problems = int(entry.get("problems_solved", 0))
+		total_problems += problems
+		rating = int(entry.get("rating", 0))
+		total_rating += rating
+		blocker = entry.get("difficulties", "")
+		if blocker:
+			total_blockers.append(blocker)
+
+	print("WEEKLY SUMMARY\n")
+	print("--------\n")
+	print(f"Total sessions logged:{total_sessions}\n")
+	print(f"Total hours studied:{total_hours}\n")
+	print(f"Total problems solved:{total_problems}\n")
+	average_rating = total_rating / total_sessions if total_sessions > 0 else 0
+	print(f"Average session rating:{average_rating}\n\n")
+	print(f"BLOCKERS THIS WEEK:\n")
+	for blockers in total_blockers:
+		print('. '+blockers)
+
+entries = load_entries()
+print_summary(entries)
 
